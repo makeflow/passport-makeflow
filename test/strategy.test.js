26161f1f04239238266befaf12a6da323c844fd0
boenfu <1997@boenfu.cn>
2020-08-17 14:18:25 +0800

    update

---
+++ test/strategy.test.js
@@ -0,0 +1,76 @@
+/* global describe, it, expect */
+/* jshint expr: true */
+
+var MakeflowStrategy = require('../lib/strategy');
+
+describe('Strategy', function () {
+  var strategy = new MakeflowStrategy(
+    {
+      clientID: 'ABC123',
+      clientSecret: 'secret',
+    },
+    function () {},
+  );
+
+  it('should be named makeflow', function () {
+    expect(strategy.name).to.equal('makeflow');
+  });
+
+  it('should have default user agent', function () {
+    expect(strategy._oauth2._customHeaders['User-Agent']).to.equal(
+      'passport-makeflow',
+    );
+  });
+
+  describe('constructed with user agent option', function () {
+    var strategy = new MakeflowStrategy(
+      {
+        clientID: 'ABC123',
+        clientSecret: 'secret',
+        userAgent: 'example.com',
+      },
+      function () {},
+    );
+
+    it('should have default user agent', function () {
+      expect(strategy._oauth2._customHeaders['User-Agent']).to.equal(
+        'example.com',
+      );
+    });
+  });
+
+  describe('constructed with custom headers including user agent', function () {
+    var strategy = new MakeflowStrategy(
+      {
+        clientID: 'ABC123',
+        clientSecret: 'secret',
+        customHeaders: {'User-Agent': 'example.net'},
+      },
+      function () {},
+    );
+
+    it('should have default user agent', function () {
+      expect(strategy._oauth2._customHeaders['User-Agent']).to.equal(
+        'example.net',
+      );
+    });
+  });
+
+  describe('constructed with both custom headers including user agent and user agent option', function () {
+    var strategy = new MakeflowStrategy(
+      {
+        clientID: 'ABC123',
+        clientSecret: 'secret',
+        customHeaders: {'User-Agent': 'example.org'},
+        userAgent: 'example.net',
+      },
+      function () {},
+    );
+
+    it('should have default user agent', function () {
+      expect(strategy._oauth2._customHeaders['User-Agent']).to.equal(
+        'example.org',
+      );
+    });
+  });
+});
